978fa1015dfff3ffedbe86d28d59db2f034484bb
Kris Ghr <krghr1995@gmail.com>
2026-05-09 01:05:31 -0400

    Updated autorun UI with UiPromptRegister natives.

--- autorun/autorun.lua
+++ autorun/autorun.lua
@@ -11,10 +11,13 @@ local currentSpeed = 1.0
 -- Controls
 local KEY_A = 0x7065027D
 local KEY_D = 0xB4E465B4
-local KEY_CTRL = GetHashKey("INPUT_DUCK")
+local KEY_CTRL = 0xDB096B85 -- LEFT CONTROL
 local KEY_TOGGLE = 0x80F28E95 -- G
 local KEY_SHIFT = 0x8FFC75D6
 local KEY_AIM = 0x07CE1E61
+
+local autoRunPromptGroupMain = GetRandomIntInRange(0, 0xffffff)
+local promptStop, promptInc, promptDec
 
 local function getSpeedLabel(speed)
     if speed <= 0.5 then
@@ -28,41 +31,39 @@ local function getSpeedLabel(speed)
     end
 end
 
-local function drawAutoRunHelp()
-    local boxRight = 0.98
-    local boxWidth = 0.18
-    local boxHeight = 0.145
-    local boxTop = 0.08
-    local padding = 0.01
+local function registerAutoRunPrompts()
+    -- Stop Autowalking (Left Click)
+    promptStop = UiPromptRegisterBegin()
+    UiPromptSetControlAction(promptStop, KEY_AIM)
+    UiPromptSetText(promptStop, CreateVarString(10, "LITERAL_STRING", "Stop Autowalking"))
+    UiPromptSetEnabled(promptStop, true)
+    UiPromptSetVisible(promptStop, true)
+    UiPromptSetStandardMode(promptStop, true)
+    UiPromptSetGroup(promptStop, autoRunPromptGroupMain, 0)
+    PromptSetPriority(promptStop, 3) -- highest
+    UiPromptRegisterEnd(promptStop)
 
-    local boxLeft = boxRight - boxWidth
+    -- Increase Speed (Shift)
+    promptInc = UiPromptRegisterBegin()
+    UiPromptSetControlAction(promptInc, KEY_SHIFT)
+    UiPromptSetText(promptInc, CreateVarString(10, "LITERAL_STRING", "Increase Speed"))
+    UiPromptSetEnabled(promptInc, true)
+    UiPromptSetVisible(promptInc, true)
+    UiPromptSetStandardMode(promptInc, true)
+    UiPromptSetGroup(promptInc, autoRunPromptGroupMain, 0)
+    PromptSetPriority(promptInc, 2) -- above decrease
+    UiPromptRegisterEnd(promptInc)
 
-    -- Background: black, roughly 20% opacity
-
-
-    -- Red title
-    SetTextScale(0.35, 0.35)
-    SetTextColor(255, 80, 80, 255)
-    SetTextJustification(2)
-    SetTextWrap(boxLeft + padding, boxRight - padding)
-
-    DisplayText(CreateVarString(10, "LITERAL_STRING",
-        "Autowalk Active"
-    ), boxRight - padding, boxTop + padding)
-
-    -- White body
-    SetTextScale(0.35, 0.35)
-    SetTextColor(255, 255, 255, 215)
-    SetTextJustification(2)
-    SetTextWrap(boxLeft + padding, boxRight - padding)
-
-    DisplayText(CreateVarString(10, "LITERAL_STRING",
-        getSpeedLabel(currentSpeed) ..
-        "\nG, Left Click, or /autorun: Cancel" ..
-        "\nShift: Increase Speed" ..
-        "\nCtrl: Decrease Speed" ..
-        "\nA/D: Turn"
-    ), boxRight - padding, boxTop + padding + 0.025)
+    -- Decrease Speed (Ctrl)
+    promptDec = UiPromptRegisterBegin()
+    UiPromptSetControlAction(promptDec, KEY_CTRL)
+    UiPromptSetText(promptDec, CreateVarString(10, "LITERAL_STRING", "Decrease Speed"))
+    UiPromptSetEnabled(promptDec, true)
+    UiPromptSetVisible(promptDec, true)
+    UiPromptSetStandardMode(promptDec, true)
+    UiPromptSetGroup(promptDec, autoRunPromptGroupMain, 0)
+    PromptSetPriority(promptDec, 1) -- below increase
+    UiPromptRegisterEnd(promptDec)  
 end
 
 local function getForwardPoint(ped, distance)
@@ -98,6 +99,7 @@ RegisterCommand("autorun", function()
 end, false)
 
 CreateThread(function()
+    registerAutoRunPrompts()
     while true do
         Wait(0)
 
@@ -106,29 +108,21 @@ CreateThread(function()
         end
 
         if autoRun then
-            drawAutoRunHelp()
+            local labelMain = CreateVarString(10, "LITERAL_STRING", "Autowalk - " .. getSpeedLabel(currentSpeed))
+            UiPromptSetActiveGroupThisFrame(autoRunPromptGroupMain, labelMain, 0, 0, 0, 0)
 
             local ped = PlayerPedId()
 
-            -- Increase speed with Shift
             if IsControlJustPressed(0, KEY_SHIFT) then
                 currentSpeed = currentSpeed + SPEED_STEP
-
                 if currentSpeed > MAX_SPEED then
                     currentSpeed = MAX_SPEED
                 end
             end
 
-            -- Disable default Ctrl actions so Ctrl only affects autorun speed
-            DisableControlAction(0, KEY_CTRL, true)
-            DisableControlAction(1, KEY_CTRL, true)
-            DisableControlAction(2, KEY_CTRL, true)
-
-            -- Decrease speed with Ctrl
-            if IsDisabledControlPressed(0, KEY_CTRL) or IsDisabledControlPressed(1, KEY_CTRL) or IsDisabledControlPressed(2, KEY_CTRL) then
+            if IsControlPressed(0, KEY_CTRL) then
                 if not speedHeld then
                     speedHeld = true
-
                     currentSpeed = currentSpeed - SPEED_STEP
 
                     if currentSpeed <= 0.0 then
